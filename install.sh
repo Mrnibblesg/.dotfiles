ca5153f55ab6eef38d15ea5ed6578d68e8abe8dd
Parker Gutierrez <parkerg444@gmail.com>
2025-07-20 01:08:56 -0700

    First dotfile

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-# Empty for now
+
+# For each file here, make link with same name in $HOME/.config?
+# Make sure an existing config isn't about to be overwritten?
+
+echo "Mrnibbles' Dotfiles"
+ln -s $PWD/nvim $HOME/.config/nvim
