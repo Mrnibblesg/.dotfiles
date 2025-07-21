1eafafd6408a9b9a3881c0276e34b01913249a8e
Parker Gutierrez <parkerg444@gmail.com>
2025-07-21 00:38:55 -0700

    Add kitty config, it's mostly default though

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 # For each file here, make link with same name in $HOME/.config?
 # Make sure an existing config isn't about to be overwritten?
+# Overwrite links if the link to be set already exists
 
 echo "Mrnibbles' Dotfiles"
 ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/kitty $HOME/.config/kitty
+
