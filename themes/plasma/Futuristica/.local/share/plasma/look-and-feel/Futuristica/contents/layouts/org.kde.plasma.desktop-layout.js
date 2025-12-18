7b08897be8a0765d3528ee7a52d58ff44f5e6cac
Parker Gutierrez <parkerg444@gmail.com>
2025-12-17 21:23:10 -0800

    Add theme helper script for better global theme switching. Setup Futuristica theme for home dir injection. Add previews

--- themes/plasma/Futuristica/.local/share/plasma/look-and-feel/Futuristica/contents/layouts/org.kde.plasma.desktop-layout.js
+++ themes/plasma/Futuristica/.local/share/plasma/look-and-feel/Futuristica/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,6 +1,58 @@
-loadTemplate("org.kde.plasma.desktop.defaultPanel")
+var plasma = getApiVersion(1);
 
-var desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+//USE THE theme.sh SCRIPT OR SET MANUALLY. NECESSARY FOR WALLPAPERS
+var home;
+
+var layout = {
+    "desktops": [
+        {
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "1051",
+                    "DialogWidth": "1912"
+                },
+                "/General": {
+                    "positions": "{\"1920x1080\":[]}"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": `file://${home}/Pictures/wallpapers/hitech/darkcube.png`,
+                    "SlidePaths": "/usr/share/wallpapers/"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        },
+        {
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "1",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "630",
+                    "DialogWidth": "810"
+                },
+                "/General": {
+                    "positions": "{\"1920x1080\":[],\"640x480\":[]}"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "FillMode": "1",
+                    "Image": `file://${home}/Pictures/wallpapers/hitech/fractal.png`,
+                    "SlidePaths": "/usr/share/wallpapers/"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "serializationFormatVersion": "1"
 }
+;
+
+plasma.loadSerializedLayout(layout);
