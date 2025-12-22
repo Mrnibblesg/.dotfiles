af2c2aef82c658c38f140f01ab40a6ae2089faed
Parker Gutierrez <parkerg444@gmail.com>
2025-12-21 17:32:12 -0800

    Prevent going out of bounds for wallpaper script

--- themes/plasma/Futuristica/.local/share/plasma/look-and-feel/Futuristica/contents/layouts/org.kde.plasma.desktop-layout.js
+++ themes/plasma/Futuristica/.local/share/plasma/look-and-feel/Futuristica/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -8,7 +8,7 @@ let wallpaperConfigs = [`file://${home}/Pictures/wallpapers/hitech/darkcube.png`
 ];
 
 var desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
+for( var j = 0; j < Math.min(desktopsArray.length, wallpaperConfigs.length); j++) {
     desktop = desktopsArray[j];
     desktop.wallpaperPlugin = 'org.kde.image';
     desktop.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
