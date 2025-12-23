c22bd928c28cc91a952f5ad6c7b22dbffc318e37
Parker Gutierrez <parkerg444@gmail.com>
2025-12-23 11:59:22 -0800

    Add to gitignore. Update shortcuts. Add kitty color scheme. Add autocommand groups for nvim

--- configs/.config/nvim/lua/config/autocmds.lua
+++ configs/.config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,14 @@
 -- [[ Basic Autocommands ]].
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, {clear=true})
+end
+
 -- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
 -- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
 -- See `:help 'clipboard'`
 vim.api.nvim_create_autocmd('UIEnter', {
+  group = augroup("clipboard"),
   callback = function()
     vim.o.clipboard = 'unnamedplus'
   end,
@@ -13,21 +19,18 @@ vim.api.nvim_create_autocmd('UIEnter', {
 -- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
+  group = augroup("yankflash`"),
   callback = function()
     vim.hl.on_yank()
   end,
 })
--- This was provided in the vim manual but we're using lua not vimscript. I have yet to translate this one TODO
-   -- augroup RestoreCursor
-   -- autocmd!
-   --   autocmd BufReadPre * autocmd FileType <buffer> ++once
-   --     \ let s:line = line("'\"")
-   --     \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
-   --     \      && index(['xxd', 'gitrebase'], &filetype) == -1
-   --     \ |   execute "normal! g`\""
-   --     \ | endif
-   -- augroup END
-   --
 
-
-
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Auto update Lazy plugins',
+  group = augroup("autoupdate"),
+  callback = function()
+    if require("lazy.status").has_updates then
+      require("lazy").update({show = false,})
+    end
+  end
+})
